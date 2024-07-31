0ce1b300e383f0d2ec2a83cbed4c8fcdcccd7f02
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-07-31 23:01:04 +0100

    updating

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-
 const express = require("express");
 const connectDB=require("./connectDB")
 const app=express()
+app.use(express.json())
 connectDB()
 const port=5000
-
+app.use("/api/person",require("./routes/personroute.js"))
 app.listen(port,(err)=>err?console.log(err):console.log("serveur is running on port 5000"))
  
 
